12c9b7ff08f390d4b9925414c37a97ef5a51ceb3
Jure Demsar <demsarjure@gmail.com>
2025-01-07 08:39:24 +0100

    Repository cleanup.

--- 9_recidiv.R
+++ 9_recidiv.R
@@ -54,12 +54,6 @@ df_recidiv <- drop_na(df_all)
 # diff
 df_recidiv$diff <- df_recidiv$recidiv - df_recidiv$procedure_date
 
-# split
-df_recidiv_close <- df_recidiv %>%
-  filter(procedure_type == "close")
-df_recidiv_high_density <- df_recidiv %>%
-  filter(procedure_type == "high_density")
-
 
 # percentage/number of recidivs between groups ---------------------------------
 report_sum_ci(df_close$had_recidiv)
@@ -77,50 +71,7 @@ prop.test(
 )
 
 
-# duration between the procedure and the recidiv -------------------------------
-report_mean_ci(df_recidiv_close$diff)
-report_mean_ci(df_recidiv_high_density$diff)
-
-
-# survival analysis ------------------------------------------------------------
-# create the survival object
-df_recidiv$status <- rep(1, nrow(df_recidiv))
-surv_obj <- Surv(time = df_recidiv$diff, event = df_recidiv$status)
-
-# fit the Kaplan-Meier model
-km_fit <- survfit(surv_obj ~ procedure_type, data = df_recidiv)
-
-# plot the Kaplan-Meier survival curves
-surv_plot <- ggsurvplot(km_fit,
-  data = df_recidiv,
-  pval = TRUE,
-  conf.int = TRUE,
-  legend.title = "",
-  legend.labs = c("CM group", "HD group"),
-  xlab = "Time (days)",
-  ylab = "Freedom from arrhythmia recurrence (%)",
-  fun = "event"
-)
-
-# extract ggplot and flip the y-axis
-surv_plot$plot +
-  scale_y_reverse(
-    breaks = c(0, 0.25, 0.5, 0.75, 1.00),
-    labels = c(100, 75, 50, 25, 0)
-  )
-
-# save as a png
-ggsave(
-  "figs/km_recidiv.png",
-  width = 1920,
-  height = 1080,
-  dpi = 300,
-  units = "px",
-  bg = "white"
-)
-
-
-# KM for all -------------------------------------------------------------------
+# KM ---------------------------------------------------------------------------
 df_all$diff <- df_all$recidiv - df_all$procedure_date
 
 df_km_all <- df_all %>%
@@ -136,7 +87,7 @@ km_all_fit <- survfit(surv_obj_all ~ procedure_type, data = df_km_all)
 
 surv_plot_all <- ggsurvplot(
   km_all_fit,
-  data = df,
+  data = df_km_all,
   pval = TRUE,
   conf.int = TRUE,
   legend.title = "",
@@ -267,77 +218,3 @@ ggsave(
   units = "px",
   bg = "white"
 )
-
-
-# is recidiv correlated with the number of reconnected veins -------------------
-df_reconnected <- df_all %>%
-  select(procedure_type, number_of_reconnected_veins, had_recidiv)
-df_reconnected <- drop_na(df_reconnected)
-df_recidiv_reconnected <- df_reconnected %>% filter(had_recidiv == 1)
-df_no_recidiv_reconnected <- df_reconnected %>% filter(had_recidiv == 0)
-
-recidiv <- df_recidiv_reconnected$number_of_reconnected_veins
-report_mean_ci(recidiv)
-
-no_recidiv <- df_no_recidiv_reconnected$number_of_reconnected_veins
-report_mean_ci(no_recidiv)
-
-wilcox.test(recidiv, no_recidiv)
-
-
-# is recidiv correlated with the number of dormants ----------------------------
-df_dormant <- df_all %>%
-  select(
-    procedure_type,
-    had_recidiv,
-    dormant_rspv_rr,
-    dormant_rspv_ra,
-    dormant_rspv_rp,
-    dormant_ripv_ra,
-    dormant_ripv_rp,
-    dormant_ripv_ri,
-    dormant_lspv_lr,
-    dormant_lspv_lrg,
-    dormant_lspv_lp,
-    dormant_lipv_la,
-    dormant_lipv_li,
-    dormant_lipv_lp
-  )
-
-df_dormant <- drop_na(df_dormant)
-df_recidiv_dormant <- df_dormant %>% filter(had_recidiv == 1)
-df_no_recidiv_dormant <- df_dormant %>% filter(had_recidiv == 0)
-
-recidiv <- c(
-  df_recidiv_dormant$dormant_rspv_rr,
-  df_recidiv_dormant$dormant_rspv_ra,
-  df_recidiv_dormant$dormant_rspv_rp,
-  df_recidiv_dormant$dormant_ripv_ra,
-  df_recidiv_dormant$dormant_ripv_rp,
-  df_recidiv_dormant$dormant_ripv_ri,
-  df_recidiv_dormant$dormant_lspv_lr,
-  df_recidiv_dormant$dormant_lspv_lrg,
-  df_recidiv_dormant$dormant_lspv_lp,
-  df_recidiv_dormant$dormant_lipv_la,
-  df_recidiv_dormant$dormant_lipv_li,
-  df_recidiv_dormant$dormant_lipv_lp
-)
-report_mean_ci(recidiv)
-
-no_recidiv <- c(
-  df_no_recidiv_dormant$dormant_rspv_rr,
-  df_no_recidiv_dormant$dormant_rspv_ra,
-  df_no_recidiv_dormant$dormant_rspv_rp,
-  df_no_recidiv_dormant$dormant_ripv_ra,
-  df_no_recidiv_dormant$dormant_ripv_rp,
-  df_no_recidiv_dormant$dormant_ripv_ri,
-  df_no_recidiv_dormant$dormant_lspv_lr,
-  df_no_recidiv_dormant$dormant_lspv_lrg,
-  df_no_recidiv_dormant$dormant_lspv_lp,
-  df_no_recidiv_dormant$dormant_lipv_la,
-  df_no_recidiv_dormant$dormant_lipv_li,
-  df_no_recidiv_dormant$dormant_lipv_lp
-)
-report_mean_ci(no_recidiv)
-
-wilcox.test(recidiv, no_recidiv)
